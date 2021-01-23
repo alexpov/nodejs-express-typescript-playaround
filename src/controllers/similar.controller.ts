6f6a502971087a84ac4d98944d7281f2b6b3cb89
Alexander Povichuk <alexander.povichuk.dell.com>
2021-01-23 17:26:06 +0200

    basic swagger + a bit of cleanup code

--- src/controllers/similar.controller.ts
+++ src/controllers/similar.controller.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
+import HttpException from '../exceptions/HttpException';
 import SimilarWordsService from '../services/similar-words.service';
 import StatsService from '../services/stats.service';
+import { isValid } from '../utils/util';
 
 class SimilarController {
   private _similarWordsService = null;
@@ -13,6 +15,10 @@ class SimilarController {
   public similar = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const { word } = req.query;
+      if (!isValid(word)) {
+        throw new HttpException(400, "Must provide 'word' query paramter");
+      }
+
       const similar = await this._similarWordsService.getSimilarWords(word as string);
       console.debug(`similar words for '${word}' -> '${JSON.stringify(similar)}'`);
       res.json({ [word as string]: similar });
--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -3,8 +3,8 @@ import { cleanEnv, port, str } from 'envalid';
 const validateEnv = () => {
   cleanEnv(process.env, {
     NODE_ENV: str(),
-    JWT_SECRET: str(),
     PORT: port(),
+    DICTIONARY_FILE_NAME: str(),
   });
 };
 
