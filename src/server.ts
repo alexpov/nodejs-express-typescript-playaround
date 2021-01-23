2d3f24a4fb9283639f2ea71bd8a8ab63d110329e
Alexander Povichuk <alexander.povichuk.dell.com>
2021-01-23 22:51:01 +0200

    added uncaughtException listener

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,15 @@ import App from './app';
 import StatsRoute from './routes/stats.route';
 import SimilarRoute from './routes/similar.route';
 import validateEnv from './utils/validateEnv';
+import { logger } from './utils/logger';
 
 validateEnv();
+
+process.on('uncaughtException', function (e) {
+  logger.error('Ouch, an unhandled exception');
+  logger.error(e instanceof Error ? e.message : e);
+  process.exit(1);
+});
 
 const app = new App([new StatsRoute(), new SimilarRoute()]);
 
