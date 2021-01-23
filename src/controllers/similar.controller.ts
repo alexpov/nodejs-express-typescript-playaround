c0785b23cb218e1905f61f29d05385262c14728d
Alexander Povichuk <alexander.povichuk.dell.com>
2021-01-23 11:45:23 +0200

    connecting between similar controller and service

--- src/controllers/similar.controller.ts
+++ src/controllers/similar.controller.ts
@@ -1,10 +1,15 @@
 import { NextFunction, Request, Response } from 'express';
+import SimilarWordsService from '../services/similar-words.service';
 
 class StatsController {
-  public similar = (req: Request, res: Response, next: NextFunction): void => {
+  private _similarWordsService = new SimilarWordsService();
+
+  public similar = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const { word } = req.query;
-      res.json({ [word as string]: [] });
+      const similar = await this._similarWordsService.getSimilarWords(word as string);
+      console.debug(`similar words for '${word}' -> '${JSON.stringify(similar)}'`);;
+      res.json({ [word as string]: similar });
     } catch (error) {
       next(error);
     }
